ecdae885ae81f79f77a574aeaa041caebcb72fcc
Ernela <fenixoffc@gmail.com>
2026-07-07 19:53:10 +0300

    Fix

---
+++ packages/backend/src/server/api/endpoints/drive/files/caption-image.ts
@@ -0,0 +1,39 @@
+import define from "../../../define.js";
+import { createWorker } from "tesseract.js";
+
+export const meta = {
+	tags: ["drive"],
+
+	requireCredential: true,
+
+	kind: "read:drive",
+
+	description: "Return caption of image",
+
+	res: {
+		type: "string",
+		optional: false,
+		nullable: false,
+	},
+} as const;
+
+export const paramDef = {
+	type: "object",
+	properties: {
+		url: { type: "string" },
+	},
+	required: ["url"],
+} as const;
+
+export default define(meta, paramDef, async (ps) => {
+	const worker = await createWorker("eng", undefined, {
+		logger: (m) => console.log(m),
+	});
+
+	const {
+		data: { text },
+	} = await worker.recognize(ps.url);
+	await worker.terminate();
+
+	return text;
+});
